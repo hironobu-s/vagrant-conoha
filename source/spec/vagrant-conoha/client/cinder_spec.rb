4372d297ec5b4071d43515262a71ed6f683c1498
Hironobu <hiro@hironobu.org>
2015-08-21 19:19:19 +0900

    cinderのカタログタイプをConoHaに合わせた

--- source/spec/vagrant-conoha/client/cinder_spec.rb
+++ source/spec/vagrant-conoha/client/cinder_spec.rb
@@ -28,7 +28,7 @@ describe VagrantPlugins::ConoHa::CinderClient do
   before :each do
     session.token = '123456'
     session.project_id = 'a1b2c3'
-    session.endpoints = { volume: 'http://cinder' }
+    session.endpoints = { volumev2: 'http://cinder' }
     @cinder_client = VagrantPlugins::ConoHa.cinder
   end
 
